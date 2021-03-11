5d6eb0911fdd74626787ce36d2e372c9e091fceb
Rafael-Guerrero-Moreira <rafael.guerrero2016@uteq.edu.ec>
2021-03-11 17:18:52 -0500

    Layout para Listado de volumenes y artículos

---
+++ app/src/main/java/com/example/examenguerrero/ArticulosVolumen.java
@@ -0,0 +1,14 @@
+package com.example.examenguerrero;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ArticulosVolumen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_articulos_volumen);
+    }
+}
